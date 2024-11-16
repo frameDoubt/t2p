2e68ccce6d8d5b1b47f1b628712f753539d06bb3
Brandon Wright <brandon.w.wright4@gmail.com>
2024-11-16 00:30:43 -0800

    add non-null assertion operator

--- src/Counter/RTranscriber.tsx
+++ src/Counter/RTranscriber.tsx
@@ -4,6 +4,32 @@ import { Readable } from 'stream'
 
 useEffect( () => {
   const run = async () => {
-    const client = new AssemblyAI( { apiKey: Bun.env.USER_API_KEY } )
+    const client = new AssemblyAI( { apiKey: Bun.env.USER_API_KEY! } )
+    const SAMPLE_RATE = 16_000
+
+    const transcriber = client.realtime.transcriber( { sampleRate: SAMPLE_RATE } )
+
+    transcriber.on( 'open', ( { sessionId } ) => {
+      console.log( `Session opened with ID: ${sessionId}` )
+    } )
+
+    transcriber.on( 'error', ( error : Error ) => {
+      console.error( 'Error:', error )
+    } )
+
+    transcriber.on( 'close', ( code : number, reason : string ) => console.log( 'Session closed:', code, reason ) )
+
+    transcriber.on( 'transcript', ( transcript : RealtimeTranscript ) => {
+      if ( !transcript.text ) {
+        return
+      }
+
+      if ( transcript.message_type === 'PartialTranscript' ) {
+        console.log( 'Partial:', transcript.text )
+      }
+      else {
+        console.log( 'Final:', transcript.text )
+      }
+    } )
   }
 }, [] )
